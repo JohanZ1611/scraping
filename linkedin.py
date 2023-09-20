0d8322f64a07f92dc44c3b419fea554d4fd1cc32
Johan Zuluaga <johan16zulu@gmail.com>
2023-09-20 11:33:47 -0500

    obtencion del csv

--- linkedin.py
+++ linkedin.py
@@ -10,10 +10,10 @@ from selenium.webdriver.edge.options import Options
 from selenium.webdriver.common.by import By
 import time
 
-#SOLO ES CAMBIAR LA URL POR LA QUE DESEES O QUE TE GENERE GOOGLE TRAS A VER REALIZADO UNA BUSQUEDA DE EMPLEO EN LINKEDIN Y EL TE HACE EL PROCESO DE EXTRACION DE OFERTAS
+# SOLO ES CAMBIAR LA URL POR LA QUE DESEES O QUE TE GENERE GOOGLE TRAS A VER REALIZADO UNA BUSQUEDA DE EMPLEO EN LINKEDIN Y EL TE HACE EL PROCESO DE EXTRACION DE OFERTAS
 
 
-url1 = 'https://www.linkedin.com/jobs/search/?currentJobId=3716433278&distance=25&f_C=22306525%2C28119221&f_E=2%2C3&f_TPR=r604800&f_WT=2%2C1&geoId=100876405&keywords=desarrollador%20web&origin=JOB_SEARCH_PAGE_JOB_FILTER&refresh=true'
+url1 = 'https://www.linkedin.com/jobs/search/?currentJobId=3712610052&distance=25&f_C=91650798%2C28119221%2C18802834&f_E=2&f_WT=2&geoId=100876405&keywords=desarrollador%20web&origin=JOB_SEARCH_PAGE_JOB_FILTER&refresh=true'
 
 # Ruta al ejecutable de Microsoft Edge WebDriver
 edge_driver_path = r'C:\\Users\\Default\\Desktop\\msedgedriver.exe'
@@ -47,7 +47,7 @@ else:
 
 empleos_disponibles = int(pd.to_numeric(elements[0].text))
 
-#Recorro mediante scroll y ejecuto la accoin de ver mas para captrar la mayor cantidad posible de datos
+# Recorro mediante scroll y ejecuto la accoin de ver mas para captrar la mayor cantidad posible de datos
 
 contador = 2
 
@@ -64,13 +64,14 @@ while contador <= 20:
         pass
         time.sleep(4)
 
-#Extraigo los datos de las ofertas y las almaceno en una lista
+# Extraigo los datos de las ofertas y las almaceno en una lista
 
 nombre_compania = []
 nombre_empleos = []
 
 try:
-    companias = driver.find_elements(By.CLASS_NAME, 'base-search-card__subtitle')
+    companias = driver.find_elements(
+        By.CLASS_NAME, 'base-search-card__subtitle')
     for compania in companias:
         nombre_compania.append(compania.text)
 
@@ -93,10 +94,8 @@ for link_oferta in link_ofertas:
     link = link_oferta.get_attribute('href')
     linklist.append(link)
 
-# Imprimir la lista de enlaces
-print(linklist)
 
-# Extraer el detalle de cada oferta 
+# Extraer el detalle de cada oferta
 
 # Lista para almacenar los detalles y los roles
 detalles = []
@@ -105,39 +104,44 @@ detalles = []
 for link in linklist:
     # Inicializa un nuevo controlador de Microsoft Edge para cada enlace
     edge_driver_path = r'C:\\Users\\Default\\Desktop\\msedgedriver.exe'
-    
+
     # Configurar las opciones de Microsoft Edge
     edge_options = Options()
     # Puedes agregar más opciones si es necesario
     edge_options.add_argument('--disable-extensions')
-    
+
     # Configurar el servicio de Microsoft Edge con la ruta al ejecutable de Microsoft Edge WebDriver
     service = Service(executable_path=edge_driver_path)
-    
+
     # Inicializar el controlador de Microsoft Edge con las opciones y el servicio
     driver = webdriver.Edge(service=service, options=edge_options)
-    
+
     # Abre una nueva ventana o pestaña y navega a la URL
     driver.get(link)
-    
+
     try:
         # Encuentra el elemento que contiene el detalle
-        detalle_oferta = driver.find_element(By.CLASS_NAME, 'show-more-less-html__markup')
-        
+        detalle_oferta = driver.find_element(
+            By.CLASS_NAME, 'show-more-less-html__markup')
+
         # Extrae el texto del elemento 'detalle_oferta'
         detalle = detalle_oferta.text.strip()
-        
+
+        # Dividir el detalle en párrafos y seleccionar el primer párrafo
+        parrafos = detalle.split('\n')
+        primer_parrafo = parrafos[0]
+
         # Verifica que el texto no sea nulo o vacío antes de agregarlo a la lista
-        if detalle:
-            detalles.append(detalle)
+        if primer_parrafo:
+            detalles.append(primer_parrafo)
         else:
             detalles.append('No disponible')
-        
+
     except Exception as e:
         print(f'Error al extraer detalle: {str(e)}')
-    
+
     time.sleep(5)
-    
+
     # Cierra el controlador actual (pestaña)
     driver.quit()
 
